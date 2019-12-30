89e5ea53176231bda7e5c426b7cee9cef94ffc59
Aleksandar_Tulic <39736298+AleksandarTulic@users.noreply.github.com>
2019-12-30 17:38:12 +0100

    Create question.cpp

---
+++ Strukture/question.cpp
@@ -0,0 +1,7 @@
+/*---------- STRUKTURA U KOJEM CUVAMO ELEMENTE UCITANE IZ DATOTEKE QUIZ.dat -------------*/
+struct QUESTION{
+    string question;
+    string ans[3];
+    int result;
+    int user_ans;
+};
