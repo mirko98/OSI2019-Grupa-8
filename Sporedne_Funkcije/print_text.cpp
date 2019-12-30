6a86f6d1598073c2e929bd830c45d7ba30f2c8bb
Aleksandar_Tulic <39736298+AleksandarTulic@users.noreply.github.com>
2019-12-30 17:31:23 +0100

    Update print_text.cpp

--- Sporedne_Funkcije/print_text.cpp
+++ Sporedne_Funkcije/print_text.cpp
@@ -1,3 +1,4 @@
+/*------------- ISPISIVANJE ODGOVARAJUCE PORUKE ---------------*/
 void print_text(string a){
     if ( a.find("VRSTA") != string::npos || a.find("GODINA") != string::npos || a.find("LOKACIJA") != string::npos ){
         a+=" KOJU STE UNIJELI NIJE VALIDNA.\nPOKUSAJTE PONOVO!!!\n\n";
