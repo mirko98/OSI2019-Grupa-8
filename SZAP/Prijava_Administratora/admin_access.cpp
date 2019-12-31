07e48db4a460e8724d2f7aeea084b68fda2463a3
DjordjeKalaba <58883141+DjordjeKalaba@users.noreply.github.com>
2019-12-31 15:15:02 +0100

    Create admin_access.cpp

---
+++ SZAP/Prijava_Administratora/admin_access.cpp
@@ -0,0 +1,62 @@
+bool admin_acess(){
+    string name;
+    char password[100];
+    string pass_buff;
+    cout<<"Unesite sljdece stavke: "<<endl;
+ 
+    do{
+        cout<<"     - Ime: ";
+        getline(cin, name, '\n');
+ 
+        if ( !check_admin_login(name) ){
+            print_text("\nIME NALOGA");
+        }
+    }while ( !check_admin_login(name) );
+ 
+    do{
+        char c;
+        int poz = 0;
+        cout<<"     - Sifra: ";
+ 
+        while (1){
+            c=getch();
+ 
+            if ( c == 13 ){
+                break;
+            }else if ( c == 8){
+                putch('\b');
+                putch(' ');
+                putch('\b');
+                poz--;
+            }
+            else{
+                password[poz++] = c;
+                putch('*');
+            }
+        }
+ 
+        password[poz] = '\0';
+ 
+        pass_buff = password;
+        if ( !check_admin_login(pass_buff) ){
+            print_text("\nSIFRU");
+        }
+    }while( !check_admin_login(pass_buff) );
+ 
+    FILE *fp = fopen("ADMIN.dat", "rb");
+ 
+    ADMIN buff;
+ 
+    while ( fread(&buff, sizeof(ADMIN), 1, fp) ){
+        string ime_k = buff.name;
+        string sifra_k = buff.password;
+ 
+        if ( ime_k == name && sifra_k == pass_buff ){
+            return true;
+        }
+    }
+ 
+    fclose(fp);
+ 
+    return false;
+}
