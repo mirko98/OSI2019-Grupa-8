17194e2481f37bbffee687d470870a7f2ff50382
Aleksandar_Tulic <39736298+AleksandarTulic@users.noreply.github.com>
2020-01-06 22:52:50 +0100

    Update convert_string.cpp

--- Sporedne_Funkcije/convert_string.cpp
+++ Sporedne_Funkcije/convert_string.cpp
@@ -2,6 +2,13 @@
 int convert_string(string &a){
     int value = 0;
     int inc = 1;
+
+    bool flag = false;
+    if ( a[0] == '-' && (int)a.length() >= 2 ){
+        a.erase(0, 1);
+        flag = true;
+    }
+
     for (int i=a.length()-1;i>=0;i--){
         if ( !isdigit(a[i]) ){
             return -1;
@@ -11,5 +18,9 @@ int convert_string(string &a){
         inc*=10;
     }
 
+    if ( flag ) {
+        a.insert(0, "-");
+        return value * (-1);
+    }
     return value;
 }
